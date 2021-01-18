b7bd7442e2d2d066c6a64e94b4ca521c5f4286ee
Yong Ching Yan <yongchingyan@gmail.com>
2021-01-18 16:07:53 +0800

    Added Content component + scss

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Counter } from './features/counter/Counter';
 import { Display } from './features/display/Display';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { MainContent } from './components/Content';
+
 import './App.css';
 
 interface Pos {
@@ -19,61 +21,13 @@ function App() {
     console.log(pos.x, pos.y)
   } 
 
-  printPos(pos);  
-
+  printPos(pos);
+  
   return (
     <div className="App">
       <Header />
-      <div id="content">
-        <Display />
-        <div id="content-wrapper"></div>
-        <div id="content-wrapper"></div>
-      </div>
-      <div className="App-header">
-        <img src={svglogo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </div>
+      <Display />
+      <MainContent />
       <Footer />
     </div>
   );
---
+++ src/components/Content.tsx
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import '../css/content.scss'
+import data  from "../asset/assets.json"
+
+interface AssetData {
+    imagePath: string;
+    format: string;
+    link: string;
+    content: Content
+}
+
+interface Content {
+    button?: string;
+    overlay?: string;
+    text?: string;
+    prompt?:  string;
+}
+
+export class MainContent extends Component<{}, { assetsStore: AssetData[] }> {
+    
+    constructor() {
+        super({})
+        this.state = {
+            assetsStore: []
+        }
+    }
+
+    componentDidMount() {
+        // this.setState({assetsStore: []})
+        let assetsStore: any[] = []
+        data.assets.map((asset, key) => {
+            import(`../asset/${asset.imagePath}`)
+            .then(image=>{
+                assetsStore[key] = {...asset, imagePath: image.default}
+                this.setState({assetsStore})
+            });
+        });
+    }
+
+    // const [imageSrc, setImage] = useState('');
+    // import("../asset/logo.png").then(image=>setImage(image.default));
+    
+    render() {
+
+        let content; 
+        content = this.state.assetsStore.map(asset => {
+
+            let temp: JSX.Element[]  = []
+            let content : Content = asset.content
+            if (Object.keys(content).length > 0){
+                
+                if ("text" in content) {
+                    temp.push(<h3>{content["text"]}</h3>)
+                }
+                if ("button" in content) {
+                    temp.push(<button>{content["button"]}</button>)
+                }
+                if ("prompt" in content){
+                    temp.push(<a className="prompt" href={asset.link}>{content["prompt"]}</a>)
+                }
+                if ("overlay" in content){
+                    temp.push(<div className="overlay"></div>)
+                }
+            }
+
+            return (
+                <div className={asset.format}>
+                    {temp}
+                    <img src={asset.imagePath}/> 
+                </div> 
+            )
+        })
+
+        const {assetsStore} = this.state
+        return (
+            <div id="content">
+                {/* <img src={imageSrc} /> */}
+                <div className="wrapper">
+                    { this.state.assetsStore.length > 0 ? 
+                        content
+                     : null}
+                {/* <div className="small-sq"> </div>
+                <div className="small-sq"> </div>
+                <div className="small-sq"> </div>
+                <div className="small-sq"> </div>
+                <div className="rect"></div>
+                <div className="small-sq"> </div>
+                <div className="small-sq"> </div>
+                <div className="small-sq"> </div>
+                <div className="small-sq"> </div>
+                <div className="rect"></div> */}
+                </div>
+            </div>
+        )
+    }
+}
