6ef2debb9b3cfa1e3a6e53f80a9f4383089baf60
Yong Ching Yan <yongchingyan@gmail.com>
2021-01-20 12:13:06 +0800

    Removed unused feature

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import displayReducer from '../features/display/displaySlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     display: displayReducer,
   },
 });
